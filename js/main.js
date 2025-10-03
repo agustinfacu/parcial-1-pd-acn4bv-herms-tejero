d989a49ca30132c31f47a6057e9d08ea860c6760
Agustin Tejero <agustin.tejero@davinci.edu.ar>
2025-10-03 16:11:34 -0300

    feat(sidebar): elimine codigo duplicado ya que ahora esta el archivo sidebar.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
 
-// selecciono los elementos del DOM
 
-// selecciono el menu hamburguesa
-const toggle = document.getElementById("menu-toggle");
-// selecciono el sidebar 
-const menu = document.getElementById("sidebar");
-
-    toggle.addEventListener("click", () => {
-        menu.classList.toggle("active");
-    });
     
