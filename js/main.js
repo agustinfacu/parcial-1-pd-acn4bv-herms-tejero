8288f063af3f78ba6e0ad9d94b4cebf478f76963
Agustin Tejero <agustin.tejero@davinci.edu.ar>
2025-10-04 18:33:28 -0300

    feat(noticias,js,css,html)Se termino con la seccion noticias agregando botones de filtros y estilos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-
-
-    
+// main.js
+// Lógica general de la web (dejar este archivo para futuras features).
+// 👉 La lógica de Noticias/Promos está en js/noticias.js
+document.addEventListener("DOMContentLoaded", () => {
+  // Por ahora no hacemos nada acá.
+});
